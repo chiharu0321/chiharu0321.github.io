6e5bab01da2239f48a0dcb01a460b6ddf591a10d
Ace Taffy <xucgphi@gmail.com>
2024-07-31 17:08:02 +0900

    fix name in vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/LandingPage/',
+  base: '/chiharu0321.github.io/',
   plugins: [vue()],
   build: {
     rollupOptions: {
